ef24497e6075020fd7430974200f91af9698622b
João <joao.braga.porto01@gmail.com>
2024-12-05 16:21:21 -0300

    Put use-case updateUserStats in right place

--- src/app/use-cases/update-user-by-battleEvent/update-user-by-battleEvent.ts
+++ src/app/use-cases/update-user-by-battleEvent/update-user-by-battleEvent.ts
@@ -16,7 +16,28 @@ class UpdateUserByBattleEvent implements IUseCase {
     event: BattleEvent;
     monster: Monster;
   }): Promise<User> {
-    return await userRepository.update({ user, event, monster });
+    const userUpdated = this.UpdateUserStats(user, event, monster);
+    return await userRepository.update({ user: userUpdated });
+  }
+
+  private UpdateUserStats(user: User, event: BattleEvent, monster: Monster) {
+    if (event.actionType === "item-use") {
+      const nameItemFromEvent = event.item.name;
+      const newQuantityOfItem = event.result.sender.newQuantity;
+
+      user.character.items.find(
+        (item) => item.name === nameItemFromEvent
+      ).quantity = newQuantityOfItem;
+    }
+
+    user.character.hp += event.result.sender.hp || 0;
+    user.character.mp += event.result.sender.mp || 0;
+
+    if (event.result.sender.isWinner) {
+      user.character.xp += monster.xp;
+    }
+
+    return user;
   }
 }
 
